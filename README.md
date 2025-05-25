ade237b81c641acca1db7725fc9b5edd403bc883
Mabel <mae@weatherelectric.xyz>
2025-05-24 23:05:26 -0700

    Added operationswamp, operationchipsweep, consolidatedmailsystems, and thepriceofreason

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 # Hour Of Arrival
 A Year Zero revival project.
 
-![63% Complete](https://progress-bar.xyz/63/?title=19/30+Restored)
+![76% Complete](https://progress-bar.xyz/76/?title=23/30+Restored)
 
 # Notice
 This repository is part of a non-commercial archival and preservation for the original Year Zero ARG.
---
+++ websites/thepriceoftreason.xyz/js/pageControl.js
@@ -0,0 +1,13 @@
+﻿// JScript File
+function getContent(num){
+    var obj = document.getElementById("pageArea");
+    var url = num + ".htm";
+    obj.src = url;  
+}
+
+
+function getParentContent(num){
+    var obj = parent.document.getElementById("pageArea");
+    var url = num + ".htm";
+    obj.src = url;  
+}
